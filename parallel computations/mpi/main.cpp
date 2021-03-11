0b29c86fcd1e049d096dbb081b91dc1a0e22ed24
Polina S <makeyourstanding@gmail.com>
2021-03-11 23:53:21 +0200

    change mem alloc

--- parallel computations/mpi/main.cpp
+++ parallel computations/mpi/main.cpp
@@ -22,53 +22,62 @@ struct point{
 int procNum; // Number of available processes
 int procRank; // Rank of current process
 
-void init(vector<point> &points, int& N, vector<point> &res, vector<pair<point, point>> &procPoints, vector<point> &procRes, int& procNP);
-void dataDist(vector<point> &points, vector<pair<point, point>> &procPoints, int N, int procNP);
-void testDist(vector<point> &points, vector<pair<point, point>> &procPoints, int N, int procNP);
-void serialSearch(vector<point> &points, vector<point> &serialRes, int N);
-void parallelSearch(vector<point> &points, vector<pair<point, point>> &procPoints, vector<point> &procRes, int N, int procNP);
-void getRes(vector<point> &procRes, vector<point> &res, int N, int procNP);
-void testRes(vector<point> &points, vector<point> &res, int N);
+void init(point* &points, pair<point, point>* &procPoints, int& N, int& NP, int& procNP);
+void dataDist(point* points, pair<point, point>* &procPoints, int N, int NP, int procNP);
+void testDist(point* points, pair<point, point>* procPoints, int N, int procNP);
+double serialSearch(point* points, point* &serialRes, int N);
+void parallelSearch(point* points, pair<point, point>* procPoints, point* &procRes, int N,  int procNP);
+void getRes(point* procRes, point* &res, int N, int NP, int RP,int procNP);
+void testRes(point* points, point* res, int N, int RP);
+void termination(point* &points, pair<point, point>* &procPoints, point* &res, point* &procRes);
 
 double triangleSqr(point A, point B, point C);
-void printPairs(vector<pair<point, point>> &procPoints, int procNP);
-void printRes(vector<point> &res);
+void printPairs(pair<point, point>* procPoints, int procNP);
+void printRes(point* res);
+void printPoint(point p);
 
 int main(int argc, char* argv[])
 {
-    vector<point> points;
-    vector<pair<point, point>> procPoints;
-    vector<point> res, procRes; // vector of 3 result points
-    int N, procNP;
+    point* points; // list of N points
+    pair<point, point>* procPoints; // list of pairs of curr procNP points
+    int N; // number of points
+    int NP; // total number of pairs of points
+    int procNP; // curr number of pairs of points
+    int RP; // number of points in res
+    point* res; // common res vector
+    point *procRes = new point[3]; // vector of 3 result points
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &procNum);
     MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
 
-    init(points, N, res, procPoints, procRes, procNP);
-    dataDist(points, procPoints, N, procNP);
+    RP = 3 * procNum;
+    res = new point[RP];
+    init(points, procPoints, N, NP, procNP);
+    dataDist(points, procPoints, N, NP, procNP);
     testDist(points, procPoints, N, procNP);
     parallelSearch(points, procPoints, procRes, N, procNP);
-    getRes(procRes, res, N, procNP);
-    cout<<"k";
-    //testRes(points, res, N);
-/*
-    if (procRank == 0) {
-        printRes(res);
-    }*/
-    //term(pMatrix, pVector, pResult, pProcRows, pProcResult);
+    getRes(procRes, res, N, NP, RP, procNP);
+    testRes(points, res, N, RP);
+    termination(points, procPoints, res, procRes);
+
     MPI_Finalize();
     return 0;
 }
 
-void init(vector<point> &points, int& N, vector<point> &res, vector<pair<point,point>> &procPoints, vector<point> &procRes, int& procNP) {
+void init(point* &points, pair<point, point>* &procPoints, int& N, int& NP,int& procNP) {
     if (procRank == 0) {
-        freopen("in.txt","r", stdin);
+        freopen("in.txt", "r", stdin);
         cin >> N;
+        points = new point [N];
+        NP = N*(N-1)/2;
         double x, y;
         for (int i=0; i<N; i++){
-            if (cin >> x >> y)
-                points.push_back(point(x, y));
+            if (cin >> x >> y){
+                points[i] = point(x, y);
+                printPoint(points[i]);
+            }
+
         }
     }
     MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
@@ -77,21 +86,22 @@ void init(vector<point> &points, int& N, vector<point> &res, vector<pair<point,p
     MPI_Type_contiguous(2, MPI_DOUBLE, &mpi_point);
     MPI_Type_commit(&mpi_point);
 
-    MPI_Bcast(&points, N, mpi_point, 0, MPI_COMM_WORLD);
+    //MPI_Bcast(&points, N, mpi_point, 0, MPI_COMM_WORLD);
 
     // Determine the number of pair of points stored on each process
-    int restNP = N*(N-1)/2;
+    int restNP = NP;
     for (int i=0; i<procRank; i++)
         restNP = restNP - restNP/(procNum-i);
     procNP = restNP/(procNum - procRank);
+
+    procPoints = new pair<point, point>[procNP];
 }
 
-void dataDist(vector<point> &points, vector<pair<point, point>> &procPoints, int N, int procNP) {
+void dataDist(point* points, pair<point, point>* &procPoints, int N, int NP, int procNP) {
     int *pSendNum = new int [procNum];// the number of elements sent to the process
     int *pSendInd = new int [procNum]; // the index of the first data element sent to the process
-    int restNP = N*(N-1)/2; // Number of rows, that haven’t been distributed yet
+    int restNP = NP; // Number of rows, that haven’t been distributed yet
 
-    restNP = N*(N-1)/2;
     procNP = restNP / procNum;
     pSendNum[0] = restNP;
     pSendInd[0] = 0;
@@ -102,12 +112,13 @@ void dataDist(vector<point> &points, vector<pair<point, point>> &procPoints, int
         pSendInd[i] = pSendInd[i-1] + pSendNum[i-1];
     }
 
-
-    vector<pair<point, point>> pairs;
+    pair<point, point>* pairs = new pair<point, point> [NP];
+    int k = 0;
     for (int i=0; i < N; i++){
         for (int j=i+1; j < N; j++) {
             pair <point, point> p = make_pair(points[i], points[j]);
-            pairs.push_back(p);
+            pairs[k] = p;
+            k++;
         }
     }
 
@@ -118,18 +129,13 @@ void dataDist(vector<point> &points, vector<pair<point, point>> &procPoints, int
     MPI_Type_contiguous(2, mpi_point, &mpi_point_pair);
     MPI_Type_commit(&mpi_point_pair);
 
-    pair<point, point> *procP = new pair<point, point> [N*(N-1)/2];
-    MPI_Scatterv(pairs.data(), pSendNum, pSendInd, mpi_point_pair, procP, pSendNum[procRank], mpi_point_pair, 0, MPI_COMM_WORLD);
-    //MPI_Scatter(pairs.data(), pairs.size(), mpi_point_pair, procP, procNP, mpi_point_pair, 0, MPI_COMM_WORLD);
-    for (int i=0; i< N*(N-1)/2 ;i++){
-        procPoints.push_back(procP[i]);
-    }
+    MPI_Scatterv(pairs, pSendNum, pSendInd, mpi_point_pair, procPoints, pSendNum[procRank], mpi_point_pair, 0, MPI_COMM_WORLD);
 
     delete [] pSendNum;
     delete [] pSendInd;
 }
 
-void testDist(vector<point> &points, vector<pair<point, point>> &procPoints, int N, int procNP) {
+void testDist(point* points, pair<point, point>* procPoints, int N, int procNP) {
     MPI_Barrier(MPI_COMM_WORLD);
     for (int i=0; i<procNum; i++) {
         if (procRank == i) {
@@ -140,12 +146,12 @@ void testDist(vector<point> &points, vector<pair<point, point>> &procPoints, int
     }
 }
 
-void serialSearch(vector<point> &points, vector<point> &serialRes, int N) {
-    int n = min(N, (int)points.size());
+double serialSearch(point* points, point* &serialRes, int N) {
     double ans = 0, currS;
-    for (int i = 0; i < n; i++){
-        for (int j=i+1; j < n; j++){
-            for (int k=j+1; k<n; k++){
+    serialRes = new point[3];
+    for (int i = 0; i < N; i++){
+        for (int j=i+1; j < N; j++){
+            for (int k=j+1; k<N; k++){
                 if (i != j && j != k && i != k) {
                     currS = triangleSqr(points[i], points[j], points[k]);
                     if (currS > ans) {
@@ -158,7 +164,9 @@ void serialSearch(vector<point> &points, vector<point> &serialRes, int N) {
             }
         }
     }
+    return ans;
 }
+
 
 double triangleSqr(point A, point B, point C) {
     double a = sqrt((C.x-B.x)*(C.x-B.x) + (C.y-B.y)*(C.y-B.y));
@@ -168,21 +176,24 @@ double triangleSqr(point A, point B, point C) {
     return sqrt(p*(p-a)*(p-b)*(p-c));
 }
 
-void printPairs(vector<pair<point, point>> &procPoints, int procNP) {
+void printPairs(pair<point, point>* procPoints, int procNP) {
     for (int i=0; i<procNP; i++){
         printf("(%f; %f) (%f; %f)\n", procPoints[i].first.x, procPoints[i].first.y,
                procPoints[i].second.x, procPoints[i].second.y);
     }
 }
 
-void printRes(vector<point> &res) {
+void printPoint(point p) {
+    printf("(%f; %f)\n", p.x, p.y);
+}
+
+void printRes(point* res) {
     for (int i=0; i< 3; i++)
         printf("(%f; %f)\n", res[i].x, res[i].y);
 }
 
-void parallelSearch(vector<point> &points, vector<pair<point, point>> &procPoints, vector<point> &procRes, int N, int procNP) {
+void parallelSearch(point* points, pair<point, point>* procPoints, point* &procRes, int N, int procNP) {
     double ans = 0, currS;
-    procRes.resize(3);
     for (int i=0; i<N; i++){
         for (int j=0; j<procNP; j++){
             point A = points[i];
@@ -194,76 +205,50 @@ void parallelSearch(vector<point> &points, vector<pair<point, point>> &procPoint
                 procRes[0] = A;
                 procRes[1] = B;
                 procRes[2] = C;
-                cout << ans << endl;
             }
         }
     }
 }
-void getRes(vector<point> &procRes, vector<point> &res, int N, int procNP) {
-    int restNP = N*(N-1)/2; // Number of rows, that haven’t been distributed yet
-    int* pReceiveNum = new int [procNum];
-    int* pReceiveInd = new int [procNum];
-    pReceiveInd[0] = 0;
-    pReceiveNum[0] = N*(N-1)/2/procNum;
-    for (int i=1; i<procNum; i++) {
-        restNP -= pReceiveNum[i-1];
-        pReceiveNum[i] = restNP/(procNum-i);
-        pReceiveInd[i] = pReceiveInd[i-1] + pReceiveNum[i-1];
-    }
-
-    MPI_Datatype mpi_point_pair;
+void getRes(point* procRes, point* &res, int N, int NP, int RP, int procNP) {
     MPI_Datatype mpi_point;
     MPI_Type_contiguous(2, MPI_DOUBLE, &mpi_point);
     MPI_Type_commit(&mpi_point);
-    MPI_Type_contiguous(2, mpi_point, &mpi_point_pair);
-    MPI_Type_commit(&mpi_point_pair);
 
-    point *pRes = new point [3];
-    cout <<"h\n";
-    MPI_Allgatherv(procRes.data(), pReceiveNum[procRank], mpi_point, pRes, pReceiveNum, pReceiveInd, mpi_point, MPI_COMM_WORLD);
-    cout <<"h\n";
-    for (int i=0; i< 3 ;i++){
-        cout << pRes[i].x << ' ';
-        res.push_back(pRes[i]);
-    }
-    // Free the memory
-    delete [] pReceiveNum;
-    delete [] pReceiveInd;
+    MPI_Allgather(procRes, 3, mpi_point, res, RP, mpi_point, MPI_COMM_WORLD);
 }
 
-void testRes(vector<point> &points, vector<point> &res, int N) {
+void testRes(point* points, point* res, int N, int RP) {
     if (procRank == 0){
-        vector<point> serialRes;
-        serialSearch(points, serialRes, N);
-        for (int i=0; i<res.size(); i++){
-            cout << i;
+        cout << "\nSerial result: \n";
+        point* serialRes;
+        double ans = serialSearch(points, serialRes, N);
+        for (int i=0; i<3; i++){
+            printPoint(serialRes[i]);
         }
+        cout << "square is "<< ans <<endl;
     }
+    double ans = 0, currS=0;
+    int t = 0;
+    for (int i=0; i<RP; i+=3){
+        currS = triangleSqr(res[i], res[i+1], res[i+2]);
+        if (currS > ans){
+            t = i - i%3;
+            ans = currS;
+        }
 
+    }
+    cout << "\nParallel result: \n";
+    printPoint(res[t]);
+    printPoint(res[t+1]);
+    printPoint(res[t+2]);
+    cout << "square is "<< ans<<endl;
 }
-/*
 
-void testRes(double* pMatrix, double* pVector, double* pResult, int rowSize, int colSize) {
-    if (procRank == 0) {
-        double* pSerialResult = calc(pMatrix, pVector, rowSize, colSize);
-        for (int i=0; i<rowSize; i++) {
-            if (pResult[i] != pSerialResult[i]) {
-                printf("The results of serial and parallel algorithms are NOT identical.\n\n");
-                return;
-            }
-        }
-        printf("The results of serial and parallel algorithms are identical.\n");
-        delete [] pSerialResult;
+void termination(point* &points, pair<point, point>* &procPoints, point* &res,point* &procRes) {
+    if (procRank == 0){
+        delete [] points;
+         delete [] res;
     }
+        delete [] procPoints;
+        delete [] procRes;
 }
-
-double* calc(double* pMatrix, double* pVector, int rowSize, int colSize) {
-    double* ans = new double[rowSize];
-    for (int i=0; i<rowSize; i++) {
-        ans[i] = 0;
-        for (int j=0; j<colSize; j++) {
-            ans[i] += pMatrix[i*colSize+j] * pVector[j];
-        }
-    }
-    return ans;
-}*/
