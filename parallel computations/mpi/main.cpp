31e3135c752b13738ee655885ba95f713c61898f
Polina S <makeyourstanding@gmail.com>
2021-03-15 16:58:26 +0200

    mpi 2

--- parallel computations/mpi/main.cpp
+++ parallel computations/mpi/main.cpp
@@ -4,7 +4,7 @@
 #include <conio.h>
 #include <time.h>
 #include <cmath>
-#include<vector>
+
 using namespace std;
 
 struct point{
@@ -27,14 +27,16 @@ void dataDist(point* points, pair<point, point>* &procPoints, int N, int NP, int
 void testDist(point* points, pair<point, point>* procPoints, int N, int procNP);
 double serialSearch(point* points, point* &serialRes, int N);
 void parallelSearch(point* points, pair<point, point>* procPoints, point* &procRes, int N,  int procNP);
-void getRes(point* procRes, point* &res, int N, int NP, int RP,int procNP);
-void testRes(point* points, point* res, int N, int RP);
+void getRes(point* procRes, point* &res, int N, int NP, int procNP);
+void testRes(point* points, point* res, int N);
 void termination(point* &points, pair<point, point>* &procPoints, point* &res, point* &procRes);
 
 double triangleSqr(point A, point B, point C);
 void printPairs(pair<point, point>* procPoints, int procNP);
 void printRes(point* res);
 void printPoint(point p);
+void getFromFile(int& N, int& NP, point* &points);
+void randomInit(int& N, int& NP, point* &points);
 
 int main(int argc, char* argv[])
 {
@@ -43,22 +45,32 @@ int main(int argc, char* argv[])
     int N; // number of points
     int NP; // total number of pairs of points
     int procNP; // curr number of pairs of points
-    int RP; // number of points in res
-    point* res; // common res vector
-    point *procRes = new point[3]; // vector of 3 result points
+    point* res; // common res vector of size RP
+    point *procRes;
+    procRes = new point[3]; // vector of 3 result points
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &procNum);
     MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
-
-    RP = 3 * procNum;
+    int RP = 3*procNum;
     res = new point[RP];
+
     init(points, procPoints, N, NP, procNP);
+
+    //double start = MPI_Wtime();
     dataDist(points, procPoints, N, NP, procNP);
     testDist(points, procPoints, N, procNP);
     parallelSearch(points, procPoints, procRes, N, procNP);
-    getRes(procRes, res, N, NP, RP, procNP);
-    testRes(points, res, N, RP);
+    getRes(procRes, res, N, NP, procNP);
+
+    /*double finish = MPI_Wtime();
+
+
+
+    if (procRank == 0) {
+        cout << "Time of execution = "<<finish-start << endl;
+    }*/
+    testRes(points, res, N);
     termination(points, procPoints, res, procRes);
 
     MPI_Finalize();
@@ -67,26 +79,19 @@ int main(int argc, char* argv[])
 
 void init(point* &points, pair<point, point>* &procPoints, int& N, int& NP,int& procNP) {
     if (procRank == 0) {
-        freopen("in.txt", "r", stdin);
-        cin >> N;
-        points = new point [N];
-        NP = N*(N-1)/2;
-        double x, y;
-        for (int i=0; i<N; i++){
-            if (cin >> x >> y){
-                points[i] = point(x, y);
-                printPoint(points[i]);
-            }
-
-        }
+        //getFromFile(N, NP, points);
+        randomInit(N, NP, points);
+        for (int i=0; i<N;i ++)
+            printPoint(points[i]);
     }
+
     MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&NP, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    //cout << N << ' '<<NP<<endl;
 
     MPI_Datatype mpi_point;
     MPI_Type_contiguous(2, MPI_DOUBLE, &mpi_point);
     MPI_Type_commit(&mpi_point);
-
-    //MPI_Bcast(&points, N, mpi_point, 0, MPI_COMM_WORLD);
 
     // Determine the number of pair of points stored on each process
     int restNP = NP;
@@ -98,24 +103,38 @@ void init(point* &points, pair<point, point>* &procPoints, int& N, int& NP,int& 
 }
 
 void dataDist(point* points, pair<point, point>* &procPoints, int N, int NP, int procNP) {
-    int *pSendNum = new int [procNum];// the number of elements sent to the process
-    int *pSendInd = new int [procNum]; // the index of the first data element sent to the process
+
+    int *pSendNum;
+    pSendNum = new int [procNum];// the number of elements sent to the process
+    int *pSendInd;
+    pSendInd = new int [procNum]; // the index of the first data element sent to the process
     int restNP = NP; // Number of rows, that haven’t been distributed yet
 
+    MPI_Datatype mpi_point;
+    MPI_Type_contiguous(2, MPI_DOUBLE, &mpi_point);
+    MPI_Type_commit(&mpi_point);
+
+    MPI_Bcast(points, N, mpi_point, 0, MPI_COMM_WORLD);
+
     procNP = restNP / procNum;
-    pSendNum[0] = restNP;
+    pSendNum[0] = procNP;
     pSendInd[0] = 0;
+    //cout << pSendNum[0] << ' ' << pSendInd[0] << endl;
     for (int i=1; i<procNum; i++) {
         restNP -= procNP;
         procNP = restNP/(procNum - i);
         pSendNum[i] = procNP;
         pSendInd[i] = pSendInd[i-1] + pSendNum[i-1];
+        cout <<"sends "<< pSendNum[i] << ' ' << pSendInd[i] << endl;
     }
 
-    pair<point, point>* pairs = new pair<point, point> [NP];
+    pair<point, point>* pairs;
+    pairs = new pair<point, point> [NP];
     int k = 0;
-    for (int i=0; i < N; i++){
+    for (int i=0; i < N-1; i++){
         for (int j=i+1; j < N; j++) {
+            if (k >= NP)
+                break;
             pair <point, point> p = make_pair(points[i], points[j]);
             pairs[k] = p;
             k++;
@@ -123,9 +142,6 @@ void dataDist(point* points, pair<point, point>* &procPoints, int N, int NP, int
     }
 
     MPI_Datatype mpi_point_pair;
-    MPI_Datatype mpi_point;
-    MPI_Type_contiguous(2, MPI_DOUBLE, &mpi_point);
-    MPI_Type_commit(&mpi_point);
     MPI_Type_contiguous(2, mpi_point, &mpi_point_pair);
     MPI_Type_commit(&mpi_point_pair);
 
@@ -192,6 +208,33 @@ void printRes(point* res) {
         printf("(%f; %f)\n", res[i].x, res[i].y);
 }
 
+void getFromFile(int& N, int& NP, point* &points) {
+    freopen("in.txt", "r", stdin);
+        cin >> N;
+        points = new point [N];
+        NP = N*(N-1)/2;
+        double x, y;
+        for (int i=0; i<N; i++){
+            if (cin >> x >> y){
+                points[i] = point(x, y);
+                printPoint(points[i]);
+            }
+
+        }
+}
+
+void randomInit(int& N, int& NP, point* &points) {
+    //srand(time(nullptr));
+
+    N = rand() % 6+1;
+    NP = N*(N-1)/2;
+    points = new point[N];
+    for (int i=0; i<N; i++){
+        points[i] = point(rand() % 10, rand()%10);
+    }
+}
+
+
 void parallelSearch(point* points, pair<point, point>* procPoints, point* &procRes, int N, int procNP) {
     double ans = 0, currS;
     for (int i=0; i<N; i++){
@@ -208,16 +251,18 @@ void parallelSearch(point* points, pair<point, point>* procPoints, point* &procR
             }
         }
     }
+    cout << procRank<<" points are " << procRes[0].x << endl;
 }
-void getRes(point* procRes, point* &res, int N, int NP, int RP, int procNP) {
+void getRes(point* procRes, point* &res, int N, int NP, int procNP) {
     MPI_Datatype mpi_point;
     MPI_Type_contiguous(2, MPI_DOUBLE, &mpi_point);
     MPI_Type_commit(&mpi_point);
-
+    int RP = 3*procNum;
     MPI_Allgather(procRes, 3, mpi_point, res, RP, mpi_point, MPI_COMM_WORLD);
+
 }
 
-void testRes(point* points, point* res, int N, int RP) {
+void testRes(point* points, point* res, int N) {
     if (procRank == 0){
         cout << "\nSerial result: \n";
         point* serialRes;
@@ -226,29 +271,31 @@ void testRes(point* points, point* res, int N, int RP) {
             printPoint(serialRes[i]);
         }
         cout << "square is "<< ans <<endl;
-    }
-    double ans = 0, currS=0;
-    int t = 0;
-    for (int i=0; i<RP; i+=3){
-        currS = triangleSqr(res[i], res[i+1], res[i+2]);
-        if (currS > ans){
-            t = i - i%3;
-            ans = currS;
-        }
 
+        ans = 0;
+        double currS=0;
+        int t = 0;
+        for (int i=0; i<3*procNum; i+=3){
+            currS = triangleSqr(res[i], res[i+1], res[i+2]);
+            if (currS > ans){
+                t = i - i%3;
+                ans = currS;
+            }
+        }
+        cout << "\nParallel result: \n";
+        printPoint(res[t]);
+        printPoint(res[t+1]);
+        printPoint(res[t+2]);
+        cout << "square is "<< ans<<endl;
     }
-    cout << "\nParallel result: \n";
-    printPoint(res[t]);
-    printPoint(res[t+1]);
-    printPoint(res[t+2]);
-    cout << "square is "<< ans<<endl;
 }
 
 void termination(point* &points, pair<point, point>* &procPoints, point* &res,point* &procRes) {
     if (procRank == 0){
         delete [] points;
-         delete [] res;
+
     }
+    delete [] res;
         delete [] procPoints;
         delete [] procRes;
 }
